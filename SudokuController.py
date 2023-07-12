ccfd4386f835abcae3b83f92e0c1c278c9fbc4ff
Jonas Bausch <jkr.bausch@gmail.com>
2023-07-12 23:48:25 +0200

    fix sudoku generation add reset button that regenerates the sudoku

--- SudokuController.py
+++ SudokuController.py
@@ -1,5 +1,74 @@
+import random
+
+
+def find_empty_cell(board):
+    for row in range(9):
+        for column in range(9):
+            if board[row][column] == 0:
+                return row, column
+    return None
+
+
+def is_valid(board, row, col, num):
+    # Check row
+    for j in range(9):
+        if board[row][j] == num:
+            return False
+
+    # Check column
+    for i in range(9):
+        if board[i][col] == num:
+            return False
+
+    # Check 3x3 box
+    start_row = (row // 3) * 3
+    start_col = (col // 3) * 3
+    for i in range(3):
+        for j in range(3):
+            if board[start_row + i][start_col + j] == num:
+                return False
+
+    return True
+
+
+def remove_values(board, difficulty):
+    # Calculate the number of cells to remove based on difficulty
+    if difficulty == 'easy':
+        cells_to_remove = 40
+    elif difficulty == 'medium':
+        cells_to_remove = 50
+    elif difficulty == 'hard':
+        cells_to_remove = 60
+    else:
+        cells_to_remove = 0
+
+    # Remove cells
+    while cells_to_remove > 0:
+        row = random.randint(0, 8)
+        col = random.randint(0, 8)
+        if board[row][col] != 0:
+            board[row][col] = 0
+            cells_to_remove -= 1
+
 
 class SudokuController:
-    def generate_game(self):
+    def generate_sudoku(self, difficulty):
         board = [[0 for _ in range(9)] for _ in range(9)]
+        self.solve_sudoku(board)
+        remove_values(board, difficulty)
         return board
+
+    def solve_sudoku(self, board):
+        empty_cell = find_empty_cell(board)
+        if not empty_cell:
+            return True
+
+        row, col = empty_cell
+        for num in range(1, 10):
+            if is_valid(board, row, col, num):
+                board[row][col] = num
+                if self.solve_sudoku(board):
+                    return True
+                board[row][col] = 0
+
+        return False
--- SudokuView.py
+++ SudokuView.py
@@ -2,28 +2,46 @@ import tkinter as tk
 from SudokuController import SudokuController
 
 
+def determine_color(row, column):
+    if (row in (0, 1, 2, 6, 7, 8) and column in (3, 4, 5)) or (
+            row in (3, 4, 5) and column in (0, 1, 2, 6, 7, 8)):
+        color = "grey"
+    else:
+        color = "white"
+    return color
+
+
 class SudokuView:
 
-    def __init__(self):
+    def __init__(self, initial_difficulty='easy'):
+        self.difficulty = initial_difficulty
         self.sudokuController = SudokuController()
-        self.master = tk.Tk()
+        self.rootView = tk.Tk()
         self.cells = {}
+        self.generate_board()
+        self.create_buttons()
+        self.rootView.mainloop()
 
     def generate_board(self):
-        self.sudokuController.generate_game()
-        print('sudoku')
-        game = self.sudokuController.generate_game()
-        row_counter = 0
-        for row in game:
-            column_counter = 0
-            for column in row:
-                if (row_counter in (0, 1, 2, 6, 7, 8) and column_counter in (3, 4, 5)) or (row_counter in (3, 4, 5) and column_counter in (0, 1, 2, 6, 7, 8)):
-                    color = "grey"
-                else:
-                    color = "white"
-                self.cells[(row_counter, column_counter)] = tk.Label(self.master, width=4, height=2, font=("Helvetica", 20), bg=color,
-                                           relief="raised")
-                self.cells[(row_counter, column_counter)].grid(row=row_counter, column=column) #TODO use actual array values
-                column_counter += 1
-            row_counter += 1
-        self.master.mainloop()
+        game = self.sudokuController.generate_sudoku(self.difficulty)
+        row = 0
+        for rows in game:
+            column = 0
+            for _ in rows:
+                self.generate_cell(game, row, column)
+                column += 1
+            row += 1
+
+    def generate_cell(self, game, row, column):
+        color = determine_color(row, column)
+        self.cells[(row, column)] = tk.Label(self.rootView, width=4, height=2, font=("Helvetica", 20), bg=color,
+                                             relief="raised")
+        if game[row][column] != 0:
+            self.cells[(row, column)].config(text=game[row][column], fg="black")
+        else:
+            self.cells[(row, column)].config(text="", fg="black")
+        self.cells[(row, column)].grid(row=row, column=column)
+
+    def create_buttons(self):
+        reset_button = tk.Button(self.rootView, text="Reset", font=("Helvetica", 16), command=self.generate_board)
+        reset_button.grid(row=10, column=5, columnspan=3, padx=5, pady=5)
